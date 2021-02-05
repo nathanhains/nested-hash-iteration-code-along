7536920f3e39d891c2214d8ac209edf4455f73ad
nathan hains <nathanhains14@yahoo.com>
2021-02-05 18:19:40 -0500

    commit tree bae33ea8af6ecf91da2c500251f56b78b414e916

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,15 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+  contacts.each do |name, value|
+    if  name == "Freddy Mercury"
+    value.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
+end
+
 
 end
